358ebf06d9054b7b16de7d1ad07699bc1fe21dc6
Hardiksharma8935 <hardikrampur440@gmail.com>
2026-07-09 10:11:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ GROUPS = {
         "stars": 133, # ₹199 / 1.5 = ~133 Stars
         "chat_id": "-1003589926855", 
         "demo": "https://t.me/DemoNovazenithXbot?start=BQADAQADjh4AAmjCeUY17uH7NGywPhYE"
-    },
+    }
     
 }
 UPI_ID = "Hardiksharma8935@fam"
